257e8dead9bc9c2c68902089d8181e34173245e6
Sandy <sandy@sndy.moe>
2022-12-27 01:07:34 -0500

    Update to 2022.1.2

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@ patch=$(curl -L 'https://github.com/sndyx/rpg-pack/blob/master/patch?raw=true')
 echo $patch >> patch
 version+="."$((1+patch))
 
+echo "{\"pack\": {\"pack_format\": 9,\"description\":\"Edition $version\"}}" >> pack.mcmeta
+
 ./zip
 git add .
 git commit -m "Update to $version"
