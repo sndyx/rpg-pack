4ddb1c0ae20cce20bd2a7bfb8a8f3ce29337c4fe
BuildTools <unconfigured@null.spigotmc.org>
2023-05-14 17:44:41 -0400

    Update to 2022.1.7

---
+++ update-windows.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+version="2022.1"
+patch=$(curl -s -L 'https://github.com/sndyx/rpg-pack/blob/master/patch?raw=true')
+rm patch
+echo $((1+patch)) >> patch
+version+="."$((1+patch))
+
+rm pack.mcmeta
+echo "{\"pack\": {\"pack_format\": 9,\"description\":\"Edition $version\"}}" >> pack.mcmeta
+
+ls
+./zip-windows.sh
+git add .
+git commit -m "Update to $version"
+git push
+
+$SHELL
